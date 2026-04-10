301693a3688bda36bd8f151bb6a6aa0762713f59
BeFucker <hpinfinikjulio@gmail.com>
2026-04-10 23:22:59 +0700

    Add files via upload

---
+++ data/songs/exotic/ASMR RAINBOW STAR COOKIES EATING with RED BALL 4 & OM NOM & ALPHABET LORE MUKBANG ANIMATION.js
@@ -0,0 +1,2 @@
+ytt="ASMR RAINBOW STAR COOKIES EATING with RED BALL 4 & OM NOM & ALPHABET LORE MUKBANG ANIMATION #Shorts",
+sinf(t*ytt.charCodeAt((t&t>>12)%ytt.length)/128,"in") +saw(100000/(t/4&t>>5)&255)/128-1
